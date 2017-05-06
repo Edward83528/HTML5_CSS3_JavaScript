ad5d2c8652583b9a055e40e8b7b7732c34851ef1
ZHONG WEI ZHANG <u0151051@gmail.com>
2017-05-06 02:14:12 +0000

    想要打的描述

---
+++ example24_AJAX/ajax.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <form>
+            <select>
+                <option value="0">a</option>
+                 <option value="1">b</option>
+                  <option value="2">c</option>
+                
+                
+            </select>
+                <select>
+                <option value="0">a1</option>
+                 <option value="1">a2</option>
+                  <option value="2">a3</option>
+                
+                
+            </select>
+            <input type="text" name=""/>
+            <input type="submit" value="送出"/>
+        </form>
+    </body>
+</html>
