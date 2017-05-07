4e5d448542f8dbd0bb1e6d4020075e56f2929660
ZHONG WEI ZHANG <u0151051@gmail.com>
2017-05-07 02:15:44 +0000

    學習

--- example24_AJAX/ajax.php
+++ example24_AJAX/ajax.php
@@ -4,23 +4,39 @@
         <meta charset="UTF-8">
     </head>
     <body>
+        <script type="text/javascript" src="jquery.min.js"></script>
+        <script type="text/javascript">
+
+$(document).ready(init);
+
+
+function init() {
+	$("#letter").change(letterChange);
+}
+
+function letterChange() {
+	// var s = $("#letter option:selected").val();
+	var s = $("#letter option:selected").text();
+	//利用jquery的get的方法可從url撈資料
+	$.get('getLetterNumber.php?letter=' + s, letterChangeDataBack)
+}
+
+function letterChangeDataBack(data) {
+	$("#letterNumber").html(data);
+}     </script>
         <form>
-            <select>
+            <select  name="letter" id="letter">
                 <option value="0">a</option>
                  <option value="1">b</option>
                   <option value="2">c</option>
-                
-                
             </select>
-                <select>
+                <select name="letterNumber" id="letterNumber">
                 <option value="0">a1</option>
                  <option value="1">a2</option>
                   <option value="2">a3</option>
-                
-                
             </select>
-            <input type="text" name=""/>
             <input type="submit" value="送出"/>
         </form>
+        	<br><div id="test"></div>
     </body>
 </html>
